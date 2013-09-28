b496b50b455a22fd4face60d309f21fa58366243
David Shuck <deshuck@gmail.com>
2013-09-28 12:35:36 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JeanApp::Application.config.secret_key_base = '50e3d6198fe0fc5b48a1a39e29207605cee8944658b25ba4b7d66e41bea3c352c25fa9bd8eb5fa9a0623533f2a68cbaa13ab3114780612090b14297a0b7e400f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JeanApp::Application.config.session_store :cookie_store, key: '_jean_app_session'
